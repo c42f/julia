bdd465b232ac673e04dbc09ce4e3ae4d0ececdf4
Chris Foster <chris42f@gmail.com>
2017-11-30 11:12:22 +1000

    Port a few missed uses of logging

--- base/pkg/entry.jl
+++ base/pkg/entry.jl
@@ -438,8 +438,8 @@ function update(branch::AbstractString, upkgs::Set{String})
                         if success
                             post_sha = string(LibGit2.head_oid(repo))
                             branch = LibGit2.branch(repo)
-                            info("Updating $pkg $branch...",
-                                prev_sha != post_sha ? " $(prev_sha[1:8]) → $(post_sha[1:8])" : "")
+                            @info "Updating $pkg $branch..." * (prev_sha != post_sha) ?
+                                  " $(prev_sha[1:8]) → $(post_sha[1:8])" : ""
                         end
                     end
                 end
@@ -585,7 +585,7 @@ function build(pkg::AbstractString, build_file::AbstractString, errfile::Abstrac
         open("$(escape_string(errfile))", "a") do f
             pkg, build_file = "$pkg", "$(escape_string(build_file))"
             try
-                info("Building \$pkg")
+                @info "Building \$pkg"
                 cd(dirname(build_file)) do
                     evalfile(build_file)
                 end
